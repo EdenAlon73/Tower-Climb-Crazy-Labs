bb30bb0d7f195e858bcedd1fcd91537701e414b5
EdenAlon73 <61311576+EdenAlon73@users.noreply.github.com>
2021-02-17 11:08:52 +0200

    Changes to Spike colors

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -35,14 +35,14 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator ScorePulse()
     {
-        for (float i = 1f; i <= 1.2f; i += 0.05f)
+        for (float i = 1f; i <= 2f; i += 0.05f)
         {
             scoreText.rectTransform.localScale = new Vector3(i, i, i);
             yield return new WaitForEndOfFrame();
         }
         scoreText.rectTransform.localScale = new Vector3(2f, 2f, 2f);
         
-        for (float i = 1.2f; i >= 1f ; i -= 0.05f)
+        for (float i = 2f; i >= 1f ; i -= 0.05f)
         {
             scoreText.rectTransform.localScale = new Vector3(i, i, i);
             yield return new WaitForEndOfFrame();
